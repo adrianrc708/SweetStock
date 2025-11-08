8d1e962d70249c7048cda5e13a80ca0396e9f599
adrianrc232 <adrian23erick@gmail.com>
2025-11-08 13:37:35 -0500

    Primer base

--- gps-sweetstock-main/BackEnd/src/main/java/com/SweetStock/api/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SweetStock.api.repository;
-
-import com.SweetStock.api.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-    Optional<Usuario> findByNombre(String nombre);
-}
